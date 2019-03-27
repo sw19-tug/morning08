66088f6eb25d4627ba740580d7447ad04b40f2a2
Adna <adna.ribo@hotmail.com>
2019-03-27 09:46:31 +0100

    SCP-001 build gradle for test [RA][PM]

--- Android Studio/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ Android Studio/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**

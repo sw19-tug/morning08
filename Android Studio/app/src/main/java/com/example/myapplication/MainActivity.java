ade00e081bbbdb66ca0744cd9e3744e6a241e1aa
Milán Pados <milan.pados@student.tugraz.at>
2019-03-27 11:06:05 +0100

    SCP-001 Button connection main [RA,PM]

--- Android Studio/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android Studio/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        // Wire up button to do stuff
+        // ..get the button
+        Button btn = (Button) findViewById(R.id.bt_tictac);
+        // ..set what happens when the user clicks.
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Log.i("MyApp","This is magic log messeage");
+            }
+        });
     }
 
 

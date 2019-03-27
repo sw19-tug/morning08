63ffe57d9e27f0afd64938e42070af0416b93ba4
Adna <adna.ribo@hotmail.com>
2019-03-27 10:20:44 +0100

    SCP-001 test done [RA,PM]

--- Android Studio/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ Android Studio/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,18 +1,20 @@
 package com.example.myapplication;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Instrumented test, which will execute on an Android device.

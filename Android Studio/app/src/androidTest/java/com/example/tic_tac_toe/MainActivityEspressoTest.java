d5b94f7264743ed6c62628f36aad4f8ae8ddb947
tamdav <tamara.david@student.tugraz.at>
2019-04-03 10:39:13 +0200

    SCP-002 Testcases [DT,BS]

--- Android Studio/app/src/androidTest/java/com/example/tic_tac_toe/MainActivityEspressoTest.java
+++ Android Studio/app/src/androidTest/java/com/example/tic_tac_toe/MainActivityEspressoTest.java
@@ -4,6 +4,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.widget.TextView;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,9 +13,11 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -42,4 +45,37 @@ public class MainActivityEspressoTest
         onView(withId(R.id.button_9)).check(matches(isDisplayed()));
 
     }
+
+
+   @Test
+    public void testButtonFunction(){
+        onView(withId(R.id.button_1)).perform(click());
+        onView(withId(R.id.button_1)).check(matches(notNullValue()));
+        onView(withId(R.id.button_1)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_2)).perform(click());
+        onView(withId(R.id.button_2)).check(matches(notNullValue()));
+        onView(withId(R.id.button_2)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_3)).perform(click());
+        onView(withId(R.id.button_3)).check(matches(notNullValue()));
+        onView(withId(R.id.button_3)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_4)).perform(click());
+        onView(withId(R.id.button_4)).check(matches(notNullValue()));
+        onView(withId(R.id.button_4)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_5)).perform(click());
+        onView(withId(R.id.button_5)).check(matches(notNullValue()));
+        onView(withId(R.id.button_5)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_6)).perform(click());
+        onView(withId(R.id.button_6)).check(matches(notNullValue()));
+        onView(withId(R.id.button_6)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_7)).perform(click());
+        onView(withId(R.id.button_7)).check(matches(notNullValue()));
+        onView(withId(R.id.button_7)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_8)).perform(click());
+        onView(withId(R.id.button_8)).check(matches(notNullValue()));
+        onView(withId(R.id.button_8)).check(matches(not(isEnabled())));
+        onView(withId(R.id.button_9)).perform(click());
+        onView(withId(R.id.button_9)).check(matches(notNullValue()));
+        onView(withId(R.id.button_9)).check(matches(not(isEnabled())));
+    }
 }
+

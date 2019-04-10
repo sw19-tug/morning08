c765fead076768e759d5217854b5513e420c0d35
somecoder11 <milan.pados@student.tugraz.at>
2019-04-10 09:36:33 +0200

    SCP-013 Time Display test [BA,PM]

---
+++ Android Studio/app/src/androidTest/java/com/example/myapplication/MainActivityTouchTheBlockTest.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+import static android.support.test.espresso.action.ViewActions.click;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+
+import com.example.R;
+import com.example.touchtheblock.TouchTheBlock;
+import org.junit.Rule;
+import org.junit.Test;
+import static android.support.test.espresso.Espresso.onView;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+public class MainActivityTouchTheBlockTest {
+
+
+        @Rule
+        public ActivityTestRule<TouchTheBlock> mainActivityTestRule = new ActivityTestRule<>(TouchTheBlock.class);
+
+        @Test
+        public void testButtonsVisible()
+        {
+            onView(withId(R.id.btnTime)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        }
+
+}

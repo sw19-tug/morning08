812a10ff6b6e72d2f6d95490f0caeda55539d41e
somecoder11 <milan.pados@student.tugraz.at>
2019-05-22 21:58:58 +0200

    Games-013 visibility and scale setting [PN,PN]

--- Android Studio/app/src/main/java/com/example/touchtheblock/TouchTheBlock.java
+++ Android Studio/app/src/main/java/com/example/touchtheblock/TouchTheBlock.java
@@ -3,13 +3,13 @@ package com.example.touchtheblock;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -35,90 +35,81 @@ public class TouchTheBlock extends AppCompatActivity {
 
 
     private String tvTimeText;
-    private float yPixPos = 0;
-    private float xPixPos = 0;
+    private float yPixPos;
+    private float xPixPos;
+
+
+
+
     private float pixHeight;
     private float pixWidth;
     private float btnPlaysize;
     private final int startTime = 2000;
     private DisplayMetrics dm = new DisplayMetrics();
-    private ViewGroup.LayoutParams params;
+    private ViewGroup.LayoutParams btnPlayParams;
+    private ViewGroup.LayoutParams btnEndParams;
+    private ViewGroup.LayoutParams tvTimerParams;
     private Random r = new Random();
     final int CHOOSE_BLOCK_COLOR = 1;
     final int CHOOSE_BACK_COLOR = 2;
     private  String blockColor = "a";
     private String backColor = "b";
+    private final CountDownTimer timer = createTimer();
+    private Clock clock;
+    private final float btnEndtvTimeScale = 0.8F;
+    private float tvTimeScale;
+    private final int navbarheight = 210;
     private boolean t = false;
 
 
 
-    private final CountDownTimer timer = createTimer();
 
-    private Clock clock;
-
-
-
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    //@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touchtheblock);
-        t = true;
 
+        tvTimeScale = (1.0F-btnEndtvTimeScale) * 0.8F*0.4F;
+        t = true;
         btnPlay = findViewById(R.id.btnPlayBox);
         btnEnd = findViewById(R.id.btnEndGame);
         btnStart = findViewById(R.id.btn_startgame);
-
         btnChooseBlock = findViewById(R.id.btnBlockCol);
         btnChooseBack = findViewById(R.id.btnBackCol);
-
         tvgamelost = (TextView) findViewById(R.id.tv_gamelost);
-
-        tvTime = findViewById(R.id.tv_time);
-        tvTime.setVisibility(View.VISIBLE);
-
-
+        tvTime = (TextView) findViewById(R.id.tv_time);
+        tvTime.setVisibility(View.INVISIBLE);
 
         btnPlay.setVisibility(View.INVISIBLE);
         btnEnd.setVisibility(View.INVISIBLE);
         tvgamelost.setVisibility(View.INVISIBLE);
 
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-        setScreenSize();
-        params = btnPlay.getLayoutParams();
-
+        setScreenSize(dm.heightPixels, dm.widthPixels);
+        btnPlayParams = btnPlay.getLayoutParams();
+        btnEndParams = btnEnd.getLayoutParams();
+        tvTimerParams = tvTime.getLayoutParams();
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                btnPlay.setVisibility(View.VISIBLE);
-                btnEnd.setVisibility(View.VISIBLE);
-                btnStart.setVisibility(View.INVISIBLE);
-                tvgamelost.setVisibility(View.INVISIBLE);
-                btnChooseBlock.setVisibility(View.INVISIBLE);
-                btnChooseBack.setVisibility(View.INVISIBLE);
-                game(pixHeight, pixWidth); //setting size of PlayBox
-
-
+                onBtnStart ();
             }
         });
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnChooseBlock.setVisibility(View.INVISIBLE);
-                btnChooseBack.setVisibility(View.INVISIBLE);
-                game(btnPlaysize, btnPlaysize);
+                onBtnPlay();
             }
         });
-
 
         btnEnd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                tvTime.setVisibility(View.INVISIBLE);
                 onBtnEnd();
-
             }
         });
 
@@ -140,10 +131,13 @@ public class TouchTheBlock extends AppCompatActivity {
                         com.example.touchtheblock.TouchTheBlockChooseColor.class);
 
                 startActivityForResult(intent, CHOOSE_BACK_COLOR);
-
             }
         });
 
+    }
+
+    public void  anfang(){
+        tvTime = (TextView) findViewById(R.id.tv_time);
     }
 
     private CountDownTimer createTimer(){
@@ -156,19 +150,41 @@ public class TouchTheBlock extends AppCompatActivity {
             }
 
             public void onFinish() {
-                tvTimeText = "";
+                tvTimeText = "out";
                 if (t)
                 {
                     tvTime.setText(tvTimeText);
+
                     endGame();
                 }
 
             }
 
 
+
         };
 
         return timer_;
+    }
+
+    public void onBtnStart (){
+        btnPlay.setVisibility(View.VISIBLE);
+        btnEnd.setVisibility(View.VISIBLE);
+        tvTime.setVisibility(View.VISIBLE);
+        btnStart.setVisibility(View.INVISIBLE);
+        tvgamelost.setVisibility(View.INVISIBLE);
+        btnChooseBlock.setVisibility(View.INVISIBLE);
+        btnChooseBack.setVisibility(View.INVISIBLE);
+        btnEndParams.height = (int) (pixHeight*btnEndtvTimeScale);
+        tvTime.setTextSize(TypedValue.COMPLEX_UNIT_PX,(pixHeight)* tvTimeScale);
+        game(pixHeight, pixWidth); //setting size of PlayBox
+    }
+
+    public void onBtnPlay(){
+        btnChooseBlock.setVisibility(View.INVISIBLE);
+        btnChooseBack.setVisibility(View.INVISIBLE);
+        timer.cancel();
+        game(btnPlaysize, btnPlaysize);
     }
 
     public void endGame(){
@@ -178,46 +194,71 @@ public class TouchTheBlock extends AppCompatActivity {
         tvgamelost.setVisibility(View.VISIBLE);
         btnChooseBlock.setVisibility(View.VISIBLE);
         btnChooseBack.setVisibility(View.VISIBLE);
+        tvTime.setVisibility(View.INVISIBLE);
+
     }
 
     public void onBtnEnd(){
+
         timer.cancel();
         timer.onFinish();
     }
 
     private void random_() {
-        this.yPixPos = (float) this.r.nextInt((int) (pixHeight - btnPlaysize - 210) + 1);
+        int randomnumber = 0;
+        while ( (int)(pixHeight*(1.0F-btnEndtvTimeScale)) > randomnumber || randomnumber > (pixHeight - btnPlaysize - navbarheight))
+        {
+
+
+            randomnumber = this.r.nextInt((int) (pixHeight - btnPlaysize - navbarheight));
+        }
+        this.yPixPos = (float) randomnumber; //this.r.nextInt((int) (pixHeight - btnPlaysize - 210- (pixHeight*0.2)) + (int)(pixHeight*0.2));
         this.xPixPos = (float) this.r.nextInt((int) (pixWidth - btnPlaysize) + 1);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private void setScreenSize() {
+    private float random_(float from, float to) {
+        int randomnumber = -1;
+        while ( (int)(from) > randomnumber || randomnumber > (int)(to))
+        {
+            randomnumber = this.r.nextInt((int) (to));
+        }
+        return (float)randomnumber;
+    }
 
-        pixHeight = dm.heightPixels;
-        pixWidth = dm.widthPixels;
+    private float random_(float to) {
+        int from = 1;
+        int randomnumber = -1;
+        while ( from > randomnumber || randomnumber > (int)(to))
+        {
+            randomnumber = this.r.nextInt((int) (to));
+        }
+        return (float)randomnumber;
+    }
 
 
+
+    //@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void setScreenSize(float heigth, float width) {
+        pixHeight = heigth;
+        pixWidth = width;
     }
 
     private void setBtnPlaySize(float btnHeight, float btnWidth) {
 
         btnPlaysize = (float) Math.sqrt((btnHeight * btnWidth) / 2);
-
-
     }
 
-    private void game(float btnHeight, float btnWidth) {
-
+    public void game(float btnHeight, float btnWidth) {
 
         setBtnPlaySize(btnHeight, btnWidth); //setting size of PlayBox
-        random_(); // setting random position
-        params.width = (int) btnPlaysize;
-        params.height = (int) btnPlaysize;
-        btnPlay.setLayoutParams(params);
+        yPixPos = random_((pixHeight*(1.0F-btnEndtvTimeScale)),(pixHeight - btnPlaysize - navbarheight)); // setting random position
+        xPixPos = random_(pixWidth - btnPlaysize);
+        btnPlayParams.width = (int) btnPlaysize;
+        btnPlayParams.height = (int) btnPlaysize;
+        btnPlay.setLayoutParams(btnPlayParams);
         btnPlay.setY(yPixPos);
         btnPlay.setX(xPixPos);
-
-
+        timer.start();
     }
 
     @Override
@@ -266,11 +307,40 @@ public class TouchTheBlock extends AppCompatActivity {
             }
         }
     }
+
+    public TextView getTvTime() {
+        return tvTime;
+    }
+
+    public float getyPixPos() {
+        return yPixPos;
+    }
+
+    public float getxPixPos() {
+        return xPixPos;
+    }
+
     public CountDownTimer getTimer() {
         return timer;
     }
 
+    public float getPixHeight() {
+        return pixHeight;
+    }
+
+    public float getPixWidth() {
+        return pixWidth;
+    }
+
+    public void logTvTimeText(){
+        Log.d("tv Time Text :  ", "somelogtext");
+    }
+
     public String getTvTimeText() {
         return tvTimeText;
+    }
+
+    public void printTvText(){
+        Log.d("kalapacs :  ",tvTimeText);
     }
 }

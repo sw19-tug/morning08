548f28ae4d7eb9cd3bda3300fb68fac8ea2f1317
somecoder11 <milan.pados@student.tugraz.at>
2019-05-22 21:16:38 +0200

    Games-013 test timer visibility [PM,PM]

--- Android Studio/app/src/androidTest/java/com/example/myapplication/MainActivityTouchTheBlockTest.java
+++ Android Studio/app/src/androidTest/java/com/example/myapplication/MainActivityTouchTheBlockTest.java
@@ -1,36 +1,47 @@
 package com.example.myapplication;
 import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
 
 import com.example.R;
-import com.example.touchtheblock.TouchTheBlock;
 
-import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 public class MainActivityTouchTheBlockTest {
 
 
-        @Rule
-        public ActivityTestRule<TouchTheBlock> mainActivityTestRule = new ActivityTestRule<>(TouchTheBlock.class);
-
         @Test
         public void testTimeVisible()
         {
-            onView(withId(R.id.tv_time)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+                onView(withId(R.id.tv_time)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+
         }
 
         @Test
-        public void testTimeSetVisible() {
-                onView(withId(R.id.tv_time)).check(matches(isDisplayed()));
-                onView(withId(R.id.tv_time)).check(matches(withText("")));
+        public void testTimeVisibleAfterBtnEnd() throws InterruptedException {
+                onView(withId(R.id.btn_startgame)).perform(click());
+
+                onView(withId(R.id.btnPlayBox)).perform(click());
+                onView(withId(R.id.tv_time)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+                onView(withId(R.id.btnEndGame)).perform(click());
+                onView(withId(R.id.tv_time)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
         }
+
+        @Test
+        public void testTimeVisibleAfterTimeout() throws InterruptedException {
+                onView(withId(R.id.btn_startgame)).perform(click());
+
+                onView(withId(R.id.btnPlayBox)).perform(click());
+                onView(withId(R.id.tv_time)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+                Thread.sleep(2500);
+                onView(withId(R.id.tv_time)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+
+        }
+
 }

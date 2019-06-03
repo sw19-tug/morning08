671968947c708592330150d15e3983171ce07103
somecoder11 <milan.pados@student.tugraz.at>
2019-06-03 18:29:17 +0200

    SCP-010 HangmanSettingsUnitTest  [DT,PM]

--- Android Studio/app/src/test/java/com/example/hangman/HangmanSettingsUnitTest.java
+++ Android Studio/app/src/test/java/com/example/hangman/HangmanSettingsUnitTest.java
@@ -2,6 +2,8 @@ package com.example.hangman;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class HangmanSettingsUnitTest {
@@ -11,6 +13,34 @@ public class HangmanSettingsUnitTest {
     public void import_all_words() {
         assertFalse(hangman.import_all_words());
     }
+    @Test
+    public void processwords() {
+        String[] words = {"apple", "banana", "cherry", "fig", "lemon", "mango", "orange", "pear"};
+        String expected = " apple, banana, cherry, fig, lemon, mango, orange, pear,";
+        assertEquals(expected, hangman.processwords(words));
+    }
 
+    @Test
+    public void splitline() {
+        String words = "apple banana cherry fig lemon mango orange pear";
+        String[] expected = {"apple", "banana", "cherry", "fig", "lemon", "mango", "orange", "pear"};
+        assertEquals(expected, hangman.splitline(words));
+    }
+
+
+
+    @Test
+    public void checkwords_for_adding() throws IOException {
+        String word = "kiwi";
+        assertTrue(hangman.checkwords_for_adding(word));
+    }
+
+
+    @Test
+    public void compare_words() {
+        String[] words = {"apple", "banana", "cherry", "fig", "lemon", "mango", "orange", "pear"};
+        String word = "apple";
+        assertTrue(hangman.compare_words(words,word));
+    }
 
 }

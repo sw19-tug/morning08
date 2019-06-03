e6a0e3d598b7228aade84b1691f14d29dd62ebc9
tamdav <tamara.david@student.tugraz.at>
2019-06-03 18:27:10 +0200

    SCP-010 Implementation Import [DT,PM]

--- Android Studio/app/src/main/java/com/example/hangman/HangmanSettings.java
+++ Android Studio/app/src/main/java/com/example/hangman/HangmanSettings.java
@@ -29,5 +29,107 @@ public class HangmanSettings extends AppCompatActivity {
         Button button_delete = findViewById(R.id.btn_delete);
         Button button_return = findViewById(R.id.btn_return);
 
+
+        import_all_words();
+
+        button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddText();
+            }
+        });
+
+        button_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DeleteText();
+            }
+        });
+
+        button_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
+
+    //import words
+    public boolean  import_all_words()  {
+
+        try {
+            Context context = this;
+            File path = context.getFilesDir();
+            File file1 = new File(path, "Fixed_words.txt");
+            TextView view1 = findViewById(R.id.lbl_fix_words);
+            view1.setText("");
+            add_import_to_view(file1,view1);
+
+            File file2 = new File(path, "Added_words.txt");
+            TextView view2 = findViewById(R.id.lbl_varying_words);
+            view2.setText("");
+            add_import_to_view(file2,view2);
+            return true;
+        } catch (Exception ex) {
+            System.out.println(ex);
+            return false;
+        }
+
+
+    }
+
+
+
+    public void add_import_to_view(File file, TextView current_view) throws IOException {
+        FileReader reader = new FileReader(file);
+        BufferedReader breader = new BufferedReader(reader);
+        StringBuilder fixed_text = new StringBuilder(current_view.getText().toString());
+        String line = breader.readLine();
+
+        if(line != null) {
+            do {
+                fixed_text.append(processwords(splitline(line)));
+
+                line = breader.readLine();
+            }
+            while (line != null);
+        }
+
+        current_view.append(fixed_text.toString());
+        reader.close();
+        breader.close();
+    }
+
+    public String processwords(String [] seperated_words){
+        StringBuilder adding_words = new StringBuilder(" ");
+        for (int i = 0; i < seperated_words.length; i++){
+            if(seperated_words[i] == null) continue;
+            if(seperated_words[i].equals(" ")) continue;
+
+            adding_words.append(seperated_words[i]).append(", ");
+        }
+        return adding_words.toString();
+    }
+
+    public String [] splitline(String line){
+
+        String[] seperated_words = line.split(" ");
+        return seperated_words;
+
+    }
+
+    //Add Text
+
+    public void AddText(){
+        //TODO - Add
+    }
+
+    //Delet Text
+
+    public void DeleteText(){
+        //TODO - Delete
+    }
+
 }
